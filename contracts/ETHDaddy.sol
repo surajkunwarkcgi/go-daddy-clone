082ca632e2a06116a5979cf9b545390efc88b619
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2022-12-19 12:28:27 +0900

    Project Setup

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract ETHDaddy {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ETHDaddy is ERC721 {
+    address public owner;
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+}
--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,34 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("ETHDaddy", () => {
+  let contract, deployer, owner1;
 
-})
+  const NAME = "suraj";
+  const SYMBOL = "ETHD";
+
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, owner1] = await ethers.getSigners();
+
+    const Contract = await ethers.getContractFactory("ETHDaddy");
+    contract = await Contract.deploy("suraj", "ETHD");
+    await contract.deployed();
+  });
+
+  describe("Deployement", () => {
+    it("has a name", async () => {
+      const result = await contract.name();
+      expect(result).to.equal(NAME);
+    });
+
+    it("sets the owner", async () => {
+      const result = await contract.owner();
+      expect(result).to.equal(deployer.address);
+    });
+  });
+});
